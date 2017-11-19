70f456fdbcd661dd42814ad525258bb6e0a14bfd
Eddie Melo <heriberto@melo.nyc>
2017-11-19 19:14:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,7 @@ end
 def move(board, player_input, character = "X")
   board[player_input] = character
 end
+
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken(board, index)
 end
