dd14c8766ba6d8aff704169e48bda6b5d461c332
Eddie Melo <heriberto@melo.nyc>
2017-11-19 19:21:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,9 +15,16 @@ def input_to_index(player_input)
 end
 
 def valid_move?(board, index)
-  index.between?(0, 8) && !position_taken(board, index)
+  if (position_taken?(board, index) == false && index.between?(0, 8))
+    true
+  elsif (position_taken?(board, index) == true)
+    false
+  else
+    false
+  end
+  #index.between?(0, 8) && !position_taken(board, index)
 end
 
 def position_taken?(board, index)
-  board[index] != " "
+  #board[index] != " "
 end
